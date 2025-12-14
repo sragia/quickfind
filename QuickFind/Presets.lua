bbaf14731b9249a13c2a51edbd8bb7d20fe30ef7
Exality <a.bedeicis@gmail.com>
2025-12-15 00:52:24 +0200

    Re-Enable most presets. Disable spell caching for now as it causes issues

--- QuickFind/Presets.lua
+++ QuickFind/Presets.lua
@@ -13,8 +13,8 @@ local cache = QF:GetModule('cache')
 ---@param self Presets
 ---@param id string
 ---@return function
-presets.spellCacheCallback = function(self, id)
-    return function(_, spellID, success)
+presets.spellCacheCallback = function (self, id)
+    return function (_, spellID, success)
         if (success) then
             local data = self:getSpellData(spellID)
             if (data) then
@@ -29,7 +29,7 @@ end
 ---@param self Presets
 ---@param spellID number
 ---@return SpellDataItem|false
-presets.getSpellData = function(self, spellID)
+presets.getSpellData = function (self, spellID)
     if (cache:hasSpell(spellID)) then return cache:getSpellData(spellID) end
 
     if (not C_Spell.DoesSpellExist(spellID)) then return false end
@@ -39,7 +39,7 @@ presets.getSpellData = function(self, spellID)
         local spellDescription = C_Spell.GetSpellDescription(spellID)
         if (not spellDescription) then
             local spell = Spell:CreateFromSpellID(spellID)
-            spell:ContinueOnSpellLoad(function()
+            spell:ContinueOnSpellLoad(function ()
                 local spellData = {
                     spellID = spellID,
                     name = spell:GetSpellName(),
@@ -63,14 +63,14 @@ presets.getSpellData = function(self, spellID)
     else
         local id = QF.utils.generateNewId(5)
         QF.handler:registerCallback('SPELL_DATA_LOAD_RESULT', id,
-                                    self:spellCacheCallback(id))
+            self:spellCacheCallback(id))
         C_Spell.RequestLoadSpellData(spellID)
         return false
     end
 end
 
 ---@param self Presets
-presets.fetchSpellCache = function(self)
+presets.fetchSpellCache = function (self)
     for _, data in pairs(QF.presets) do
         if (data.type == QF.LOOKUP_TYPE.SPELL) then
             for _, spellID in pairs(data.data) do
@@ -91,7 +91,7 @@ end
 
 ---Build preset suggestions
 ---@param self Presets
-presets.build = function(self)
+presets.build = function (self)
     for name, data in pairs(QF.presets) do
         QF.builtPresets[name] = QF.builtPresets[name] or {}
         if (data.type == QF.LOOKUP_TYPE.SPELL) then
@@ -115,7 +115,7 @@ presets.build = function(self)
                         C_MountJournal.GetMountInfoByID(mountID)
                     if (isUsable) then
                         QF.builtPresets[name][name .. spellID] = addPresetInfo(
-                                                                     {
+                            {
                                 icon = icon,
                                 name = mountName,
                                 mountName = mountName,
@@ -158,8 +158,8 @@ presets.build = function(self)
                 local built = data.getBuiltData()
                 for _, data in pairs(built) do
                     QF.builtPresets[name][name .. data.id] = addPresetInfo(data,
-                                                                           data.id,
-                                                                           name)
+                        data.id,
+                        name)
                 end
             end
         end
@@ -170,7 +170,7 @@ end
 
 ---@param self Presets
 ---@param preset table
-presets.addToData = function(self, preset)
+presets.addToData = function (self, preset)
     local id = QF.utils.generateNewId()
     QF:SaveData(id, QF.utils.shallowCloneMerge(preset, {
         id = id,
@@ -186,10 +186,12 @@ end
 ---@param presetID string
 ---@param presetName string
 ---@return boolean
-presets.isAddedToData = function(self, presetID, presetName)
+presets.isAddedToData = function (self, presetID, presetName)
     for _, v in pairs(QF.data) do
         if (v.presetName and v.presetName == presetName and v.presetID ==
-            presetID) then return true end
+                presetID) then
+            return true
+        end
     end
     return false
 end
@@ -198,7 +200,7 @@ end
 ---@param self Presets
 ---@param presetName string
 ---@param presetID string
-presets.setDisabledPreset = function(self, presetName, presetID)
+presets.setDisabledPreset = function (self, presetName, presetID)
     QF.disabledPresets[presetName] = QF.disabledPresets[presetName] or {}
     QF.disabledPresets[presetName][presetID] = true
     QF.disabledPresets:SetValue(presetName, QF.disabledPresets[presetName])
@@ -209,15 +211,15 @@ end
 ---@param presetName string
 ---@param presetID string
 ---@return boolean
-presets.isDisabledPreset = function(self, presetName, presetID)
+presets.isDisabledPreset = function (self, presetName, presetID)
     return QF.disabledPresets[presetName] and
-               QF.disabledPresets[presetName][presetID]
+        QF.disabledPresets[presetName][presetID]
 end
 
 ---Reset all or specific disabled presets
 ---@param self Presets
 ---@param presetName? string
-presets.resetDisabled = function(self, presetName)
+presets.resetDisabled = function (self, presetName)
     if presetName then
         QF.disabledPresets:SetValue(presetName, {})
     else
@@ -233,37 +235,37 @@ end
 ---@param self Presets
 ---@param presetName string
 ---@return boolean
-presets.hasAnyDisabled = function(self, presetName)
+presets.hasAnyDisabled = function (self, presetName)
     return QF.disabledPresets[presetName] and
-               not QF.utils.isEmpty(QF.disabledPresets[presetName])
+        not QF.utils.isEmpty(QF.disabledPresets[presetName])
 end
 
 ---Get available presets
 ---@param self Presets
 ---@return table
-presets.getAvailable = function(self)
+presets.getAvailable = function (self)
     local available = {}
     for name, data in pairs(QF.presets) do
-        table.insert(available, {name = name, description = data.description})
+        table.insert(available, { name = name, description = data.description })
     end
 
     return available
 end
 
 ---@param self Presets
-presets.init = function(self)
-    -- self:fetchSpellCache()
+presets.init = function (self)
+    --self:fetchSpellCache()
     -- Build at least initial presets
     -- Add some arbitrary delay cuz i cba
-    -- C_Timer.After(2, function() presets:build() end)
+    C_Timer.After(5, function () presets:build() end)
 end
 
 ---@param self Presets
 ---@param name string
-presets.enable = function(self, name) QF.enabledPresets[name] = true end
+presets.enable = function (self, name) QF.enabledPresets[name] = true end
 
 ---@param self Presets
 ---@param name string
-presets.disable = function(self, name) QF.enabledPresets[name] = false end
+presets.disable = function (self, name) QF.enabledPresets[name] = false end
 
-presets.isEnabled = function(self, name) return QF.enabledPresets[name] end
+presets.isEnabled = function (self, name) return QF.enabledPresets[name] end
